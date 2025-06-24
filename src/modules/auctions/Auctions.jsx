339f37085d7359189a4aeebaae8c27b2cbcbbc67
Ahmed Elkwawy <151570210+elkwawy@users.noreply.github.com>
2025-06-24 23:35:13 +0300

    Updata Project

--- src/modules/auctions/Auctions.jsx
+++ src/modules/auctions/Auctions.jsx
@@ -1,15 +1,15 @@
-import { Table, Space, Spin, Input, Image } from "antd";
-import CreateAuction from "./CreateAuction";
-import { useGetAuctionsHook } from "./hooks/useGetAuctionsHook";
-import UpdateAuction from "./UpdateAuction";
-import DeleteAuction from "./DeleteAuction";
-import { useState } from "react";
-import { SearchOutlined } from "@ant-design/icons";
-import { useGetCategoriesHook } from "../categories/hooks/useGetCategoriesHook";
+import { Table, Space, Spin, Input, Image } from 'antd';
+import CreateAuction from './CreateAuction';
+import { useGetAuctionsHook } from './hooks/useGetAuctionsHook';
+import UpdateAuction from './UpdateAuction';
+import DeleteAuction from './DeleteAuction';
+import { useState } from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+import { useGetCategoriesHook } from '../categories/hooks/useGetCategoriesHook';
 
 function Auctions() {
   const { auctions, isLoading, isFetching, error } = useGetAuctionsHook();
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
   const filteredAuctions = auctions.filter((auction) =>
     auction.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
@@ -21,33 +21,35 @@ function Auctions() {
     isLoading: categoriesIsLoading,
     error: categoriesError,
   } = useGetCategoriesHook();
+  console.log(categories);
 
   console.log(auctions);
 
   const columns = [
     {
-      title: "Image",
-      dataIndex: "images",
-      key: "images",
+      title: 'Image',
+      dataIndex: 'images',
+      key: 'images',
       render: (images) =>
         images ? (
-          <Image width={50} src={images[0]} alt="Auction Img" />
+          <Image width={50} src={images[0]} alt='Auction Img' />
         ) : (
-          "No Image"
+          'No Image'
         ),
     },
     {
-      title: "Title",
-      dataIndex: "title",
-      key: "title",
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
     },
     {
-      title: "Category",
-      dataIndex: "category_id",
-      key: "category",
+      title: 'Category',
+      dataIndex: 'category_id',
+      key: 'category',
       render: (categoryId) => {
-        const category = categories.find((cat) => cat.id === categoryId);
-        return category ? category.name : "Unknown Category";
+        const category = categories.find((cat) => cat.id === +categoryId);
+
+        return category ? category.name : 'Unknown Category';
       },
     },
     // {
@@ -61,9 +63,9 @@ function Auctions() {
     //   key: "starting_price",
     // },
     {
-      title: "Current Price",
-      dataIndex: "current_price",
-      key: "current_price",
+      title: 'Current Price',
+      dataIndex: 'current_price',
+      key: 'current_price',
     },
     // {
     //   title: "Start Time",
@@ -71,13 +73,13 @@ function Auctions() {
     //   key: "start_time",
     // },
     {
-      title: "End Time",
-      dataIndex: "end_time",
-      key: "end_time",
+      title: 'End Time',
+      dataIndex: 'end_time',
+      key: 'end_time',
     },
     {
-      title: "Actions",
-      key: "actions",
+      title: 'Actions',
+      key: 'actions',
       render: (_, record) => (
         <Space>
           <UpdateAuction auction={record} />
@@ -88,26 +90,26 @@ function Auctions() {
   ];
 
   return (
-    <section className="-mt-[17px]">
-      <h1 className="text-2xl font-bold">Auctions</h1>
-      <div className="search-add-container">
+    <section className='-mt-[17px]'>
+      <h1 className='text-2xl font-bold'>Auctions</h1>
+      <div className='search-add-container'>
         <Input
-          prefix={<SearchOutlined className="text-gray-400 mr-2" />}
-          placeholder="Search by title..."
+          prefix={<SearchOutlined className='text-gray-400 mr-2' />}
+          placeholder='Search by title...'
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           allowClear
-          className="search-input"
+          className='search-input'
         />
 
         <CreateAuction />
       </div>
 
-      {error && <p className="text-red-500">Error: {error.message}</p>}
+      {error && <p className='text-red-500'>Error: {error.message}</p>}
 
       {isLoading || categoriesIsLoading ? (
-        <div className="flex justify-center items-center h-[55vh]">
-          <Spin size="large" />
+        <div className='flex justify-center items-center h-[55vh]'>
+          <Spin size='large' />
         </div>
       ) : (
         <Table
@@ -115,9 +117,9 @@ function Auctions() {
           dataSource={filteredAuctions}
           pagination={{ pageSize: 10 }}
           scroll={{ x: 800 }}
-          className="overflow-auto"
+          className='overflow-auto'
           loading={isFetching}
-          rowKey="id"
+          rowKey='id'
         />
       )}
     </section>
